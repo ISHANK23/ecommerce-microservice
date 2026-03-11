6c6ce9fb016e4cc1e56fd57629b3461b7c729f2e
Ishan <it20247218@my.sliit.lk>
2026-03-11 20:24:11 +0530

    first commit

---
+++ services/analytic-service/index.js
@@ -0,0 +1,30 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "analytic-service",
+  brokers: ["localhost:9094"],
+});
+
+const consumer = kafka.consumer({ groupId: "analytic-service-group" });
+
+const run = async () => {
+  try {
+    await consumer.connect();
+    await consumer.subscribe({
+      topic: "payment-successful",
+      fromBeginning: true,
+    });
+
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        const value = message.value.toString();
+        const { userId, cart } = JSON.parse(value);
+        console.log(`Received message from topic ${topic}:`, { userId, cart });
+      },
+    });
+  } catch (err) {
+    console.error("Error connecting to Kafka:", err);
+  }
+};
+
+run();
---
+++ services/payment-service/index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import cors from "cors";
+import { Kafka } from "kafkajs";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  }),
+);
+
+app.use(express.json());
+
+const kafka = new Kafka({
+  clientId: "payment-service",
+  brokers: ["localhost:9094"],
+});
+
+const producer = kafka.producer();
+
+const connectToKafka = async () => {
+  try {
+    await producer.connect();
+    console.log("Connected to Kafka successfully");
+  } catch (err) {
+    console.error("Error connecting to Kafka:", err);
+  }
+};
+
+app.post("/payment-service", async (req, res) => {
+  const { cart } = req.body;
+
+  const userId = "123";
+
+  await producer.send({
+    topic: "payment-successful",
+    messages: [
+      {
+        value: JSON.stringify({
+          userId,
+          cart,
+        }),
+      },
+    ],
+  });
+
+  console.log("API endpoint hit");
+
+  return res.status(200).send("Payment successful");
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send(err.message);
+});
+
+app.listen(8000, () => {
+  connectToKafka();
+  console.log("Payment service is running on port 8000");
+});
